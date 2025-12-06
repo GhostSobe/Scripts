fcd4963ce692d5aa55bd1b8bc31e472351aca365
Ghost <jirapat2957@gmail.com>
2025-12-07 05:16:40 +0700

    Add files via upload

---
+++ License.lua
@@ -0,0 +1,13 @@
+local Licenses = {
+    "AF3aPdfKWFgYbH0iitC962pgnsx7C9gN",
+    "7eUy8fwEdbyHwGPeFgUb9BqsFmtdlzDx",
+    "yJpVkRxNwsTaeKjsuN8EyAGbJYDJA8DK",
+    "1LBiUowz4A2INnp4A1Ilk16AajkFr5EG",
+    "EnHCCGcVSFNHug46PybiUbKomFpAm76c",
+    "2JZhPIcogMzWvbeIY3Qi8jvvB2lUSJwU",
+    "oFeLGymKHPaa1NGGl9fPirmHGBH15Rom",
+    "ZckzxRXaZl5jjFXE4GL0Qzo4GOiERKZ8",
+    "agS1IktyGVDVIkPJPA6elh9tlcHWeXAm",
+    "lpiSfWGHPWUVXxlq4HBWN1WbaV4LEYH4",
+}
+return Licenses
